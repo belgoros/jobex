3f086cf0f1e5cd044868060ea3460d44e37ca6f2
belgoros <s.cambour@gmail.com>
2025-04-17 18:15:34 +0200

    Create Company Index module and route

--- lib/jobex_web/router.ex
+++ lib/jobex_web/router.ex
@@ -18,6 +18,7 @@ defmodule JobexWeb.Router do
     pipe_through :browser
 
     live "/", HomeLive
+    live "/companies", SourceLive.CompanyLive.Index
   end
 
   # Other scopes may use custom stacks.
