078d4a17a8e97b49b2d22311cec8fe9d073ca1eb
belgoros <s.cambour@gmail.com>
2025-04-17 18:15:07 +0200

    Add faker

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Jobex.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:petal_components, "~> 3.0"}
+      {:petal_components, "~> 3.0"},
+      {:faker, "~> 0.18.0", only: [:dev, :test]}
     ]
   end
 
