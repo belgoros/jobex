d7c54eeba2e3f2b6bb140e8e1f0b798c7f88574e
belgoros <s.cambour@gmail.com>
2025-04-18 22:51:51 +0200

    Update config - remove tailwind.config.js - change Path.expand

--- config/config.exs
+++ config/config.exs
@@ -46,11 +46,10 @@ config :tailwind,
   version: "4.0.9",
   jobex: [
     args: ~w(
-      --config=tailwind.config.js
-      --input=css/app.css
-      --output=../priv/static/assets/app.css
+     --input=assets/css/app.css
+      --output=priv/static/assets/app.css
     ),
-    cd: Path.expand("../assets", __DIR__)
+    cd: Path.expand("..", __DIR__)
   ]
 
 # Configures Elixir's Logger
