0990a9b87aeb33fd115842491c8f67cfec8a2086
belgoros <s.cambour@gmail.com>
2025-04-18 16:29:06 +0200

    Create a test for listing companies

---
+++ test/jobex_web/live/company_live_test.exs
@@ -0,0 +1,21 @@
+defmodule JobexWeb.CompanyLiveTest do
+  use JobexWeb.ConnCase
+  import Phoenix.LiveViewTest
+  import Jobex.SourcesFixtures
+
+  defp create_company(_) do
+    company = company_fixture()
+    %{company: company}
+  end
+
+  describe "Index" do
+    setup [:create_company]
+
+    test "lists all companies", %{conn: conn, company: company} do
+      {:ok, _index_live, html} = live(conn, ~p"/companies")
+
+      assert html =~ "Listing Companies"
+      assert html =~ company.name
+    end
+  end
+end
