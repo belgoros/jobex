7268252b6524d78f3486954e7856b5034c0029af
belgoros <s.cambour@gmail.com>
2025-04-21 13:39:20 +0200

    Generate contacts live resources, add routes

---
+++ lib/jobex_web/live/contact_live/index.ex
@@ -0,0 +1,47 @@
+defmodule JobexWeb.ContactLive.Index do
+  use JobexWeb, :live_view
+
+  alias Jobex.Sources
+  alias Jobex.Sources.Contact
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, stream(socket, :contacts, Sources.list_contacts())}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :edit, %{"id" => id}) do
+    socket
+    |> assign(:page_title, "Edit Contact")
+    |> assign(:contact, Sources.get_contact!(id))
+  end
+
+  defp apply_action(socket, :new, _params) do
+    socket
+    |> assign(:page_title, "New Contact")
+    |> assign(:contact, %Contact{})
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Listing Contacts")
+    |> assign(:contact, nil)
+  end
+
+  @impl true
+  def handle_info({JobexWeb.ContactLive.FormComponent, {:saved, contact}}, socket) do
+    {:noreply, stream_insert(socket, :contacts, contact)}
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    contact = Sources.get_contact!(id)
+    {:ok, _} = Sources.delete_contact(contact)
+
+    {:noreply, stream_delete(socket, :contacts, contact)}
+  end
+end
