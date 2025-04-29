d06a79f91a781a37257123e2560fa8173045106c
belgoros <s.cambour@gmail.com>
2025-04-29 14:44:36 +0200

    Alias Reply

--- .iex.exs
+++ .iex.exs
@@ -2,7 +2,7 @@ alias Jobex.Repo
 alias Jobex.Sources
 alias Jobex.Sources.{Company, Contact}
 alias Jobex.Applications
-alias Jobex.Applications.Position
+alias Jobex.Applications.{Position, Reply}
 
 import Ecto.Query
 import Ecto.Changeset
