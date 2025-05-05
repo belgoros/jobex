1533b87bc9a937def82de97a86a42719f46fead6
belgoros <s.cambour@gmail.com>
2025-05-05 08:33:53 +0200

    Remove border from go_forward_badge component

--- lib/jobex_web/components/custom_components.ex
+++ lib/jobex_web/components/custom_components.ex
@@ -7,9 +7,9 @@ defmodule JobexWeb.CustomComponents do
   def go_forward_badge(assigns) do
     ~H"""
     <p class={[
-      "inline-block px-2 py-1 text-xs font-medium uppercase border rounded-md",
-      @go_forward == true && "text-green-600 border-green-600",
-      @go_forward == false && "text-red-600 border-red-600",
+      "px-2 py-1",
+      @go_forward == true && "text-green-600",
+      @go_forward == false && "text-red-600",
       @class
     ]}>
       <.icon name="hero-shield-check" class="w-5 h-5" />
